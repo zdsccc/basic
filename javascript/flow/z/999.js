d177b9eb524ae0973fedef559a8aa9c076ac391c
yesman <zdsccc@gmail.com>
2018-08-27 10:26:01 +0800

    basic

--- javascript/flow/z/999.js
+++ javascript/flow/z/999.js
@@ -1,9 +1,22 @@
+// var num = 0;
+// for (var i = 1; i < 10; i++) {
+//     if (i % 5 == 0) {
+//         break;
+//         // continue;
+//     }
+//     num++;
+// }
+// console.log(num);// 4
+
 var num = 0;
-for (var i = 1; i < 10; i++) {
-    if (i % 5 == 0) {
-        break;
+outermost:
+    for (var i = 0; i < 10; i++) {
+        for (var j = 0; j < 10; j++) {
+            if (i == 5 && j == 5) {
+                continue outermost;
+            }
+            num++;
+        }
     }
-    num++;
-}
-console.log(num);// 4
+console.log(num);// 95
 
